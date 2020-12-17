5e7c53797eedf34e2074964ba90013f019b52029
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-12-17 12:06:34 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
-
-let array = [];
+var tab = [];
 function ajouterBiere(formulaire){
-    array.push( [formulaire.nom.value, formulaire.style.value,
-        formulaire.type.value, formulaire.alcool.value, formulaire.creation.value, formulaire.perimer.value] );
-    console.log(arry); 
+    tab.push( [[formulaire.nom.value, formulaire.style.value,
+        formulaire.type.value, formulaire.alcool.value, formulaire.creation.value, formulaire.perimer.value]] );
+    console.log(tab);
 }
